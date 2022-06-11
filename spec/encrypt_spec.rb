0e03c392c9d151d01b178a6d62b1cdff592e85d6
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-06-11 13:13:42 -0700

    Test/updated attibutes test and added test for checking to build ciphers

--- spec/encrypt_spec.rb
+++ spec/encrypt_spec.rb
@@ -2,6 +2,7 @@ require 'simplecov'
 SimpleCov.start
 require './lib/keys'
 require './lib/encrypt'
+require 'pry'
 
 RSpec.describe Encrypt do
 
@@ -17,5 +18,14 @@ RSpec.describe Encrypt do
     expect(encrypt.alphabet[0]).to eq("a")
     expect(encrypt.alphabet[7]).to eq("h")
     expect(encrypt.alphabet[26]).to eq(" ")
+    expect(encrypt.keys).to be_a(Keys)
+  end
+
+  it "can encrypt a message" do
+    expect(encrypt.rotate_shift(1)).to be_a(Hash)
+    expect(encrypt.rotate_shift(0)["z"]).to eq("z")
+    expect(encrypt.rotate_shift(1)["b"]).to eq("c")
+    expect(encrypt.rotate_shift(2)["d"]).to eq("f")
+    expect(encrypt.rotate_shift(3)["g"]).to eq("j")
   end
 end
