9ac8c4794c5192d8482f3ab9549f6bef936a5b74
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-06-11 10:58:00 -0700

    Test/added test for attributes of encrypt class

--- spec/encrypt_spec.rb
+++ spec/encrypt_spec.rb
@@ -8,7 +8,14 @@ RSpec.describe Encrypt do
   let(:encrypt) { Encrypt.new }
 
   it "exists" do
-    expect(encrypt).to be_a(Encrypt)  
+    expect(encrypt).to be_a(Encrypt)
   end
 
+  it "has attributes" do
+    expect(encrypt.alphabet).to be_a(Array)
+    expect(encrypt.alphabet.count).to eq(27)
+    expect(encrypt.alphabet[0]).to eq("a")
+    expect(encrypt.alphabet[7]).to eq("h")
+    expect(encrypt.alphabet[26]).to eq(" ")
+  end
 end
