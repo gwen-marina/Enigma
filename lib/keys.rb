c58e29c622ec555ad96690bfe7bb70e4630e6c5c
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-06-11 10:45:59 -0700

    Feat/wrote code to split random 5 digit number into keys

--- lib/keys.rb
+++ lib/keys.rb
@@ -7,5 +7,17 @@ class Keys
     end
   end
 
- 
+  def key_split
+    #1 key = first two digits
+    #2 key = second and third digits
+    #3 key = third and fourth digits
+    #4 key = fourth and fifth digits
+    numbers = random_key_generator
+    split_1 = numbers[0] + numbers[1]
+    split_2 = numbers[1] + numbers[2]
+    split_3 = numbers[2] + numbers[3]
+    split_4 = numbers[3] + numbers[4]
+    [split_1, split_2, split_3, split_4]
+  end
+
 end
