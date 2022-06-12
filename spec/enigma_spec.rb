d2a017b7be9c5a8f914ecdc777723f6f4d6368ff
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-06-11 19:50:44 -0700

    Test/wrote test for decrypting a message

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -12,11 +12,19 @@ RSpec.describe Enigma do
     expect(enigma).to be_a(Enigma)
   end
 
-  it "can encrypt" do
+  it "can encrypt a message" do
     expect(enigma.encrypt("hello")).to be_a(Hash)
     expect(enigma.encrypt("hello", "23456", "010385")).to be_a(Hash)
     expect(enigma.encrypt("hello")[:encryption]).to be_a(String)
     expect(enigma.encrypt("hello", "23456", "010385")[:date]).to eq("010385")
     expect(enigma.encrypt("hello", "23456", "010385")[:key]).to eq("23456")
+  end
+
+  it "can decrypt a message" do
+    encrypted_message = enigma.encrypt("hello", "23456", "010385")[:encryption]
+    expect(enigma.decrypt(encrypted_message, "23456", "010385")).to be_a(Hash)
+    expect(enigma.decrypt(encrypted_message, "23456", "010385")[:decryption]).to eq("hello")
+    expect(enigma.decrypt("hello", "23456", "010385")[:date]).to eq("010385")
+    expect(enigma.decrypt("hello", "23456", "010385")[:key]).to eq("23456")
   end
 end
