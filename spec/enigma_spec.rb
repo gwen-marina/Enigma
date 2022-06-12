e40504b18aaa6d75bad63d9fe68cd14ed40b8cbb
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-06-11 18:21:21 -0700

    Test/added test for encrypt method

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -10,7 +10,15 @@ RSpec.describe Enigma do
   let(:enigma) { Enigma.new }
 
   it "exists" do
-    expect(enigma).to be_a(Enigma)  
+    expect(enigma).to be_a(Enigma)
+  end
+
+  it "can encrypt" do
+    expect(enigma.encrypt("hello")).to be_a(Hash)
+    expect(enigma.encrypt("hello", "23456", "010385")).to be_a(Hash)
+    expect(enigma.encrypt("hello")[:encryption]).to be_a(String)
+    expect(enigma.encrypt("hello", "23456", "010385")[:date]).to eq("010385")
+    expect(enigma.encrypt("hello", "23456", "010385")[:key]).to eq("23456")
   end
 
 end
